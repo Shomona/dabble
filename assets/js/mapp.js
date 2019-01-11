740591a74d4ee248b44094afb3ed210d35e17d0d
Shomona <shomona.mukherjee@gmail.com>
2019-01-11 16:37:15 +0530

    owl fixes

--- assets/js/mapp.js
+++ assets/js/mapp.js
@@ -19,34 +19,52 @@
   // ... other parameters
 });
 */
+var fixOwl = function() {
+  var $stage = $('.owl-stage'),
+    stageW = $stage.width(),
+    $el = $('.owl-item'),
+    elW = 0;
+  $el.each(function() {
+    elW += $(this).width() + +($(this).css("margin-right").slice(0, -2))
+  });
+  if (elW > stageW) {
+    $stage.width(elW);
+  };
+}
+
+
 var owl = $('.owl-carousel.project');
 owl.owlCarousel({
-  items:4,
-  loop:true,
-  margin:10,
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoWidth: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  //onInitialized: fixOwl,
+  //onRefreshed: fixOwl
 });
-$('.play').on('click',function(){
-  owl.trigger('play.owl.autoplay',[3000])
+$('.play').on('click', function() {
+  owl.trigger('play.owl.autoplay', [3000])
 })
-$('.stop').on('click',function(){
+$('.stop').on('click', function() {
   owl.trigger('stop.owl.autoplay')
 })
 
 var owl = $('.owl-carousel.dabble');
 owl.owlCarousel({
-  items:4,
-  loop:true,
-  margin:10,
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoWidth: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true
 });
-$('.play').on('click',function(){
-  owl.trigger('play.owl.autoplay',[3000])
+$('.play').on('click', function() {
+  owl.trigger('play.owl.autoplay', [3000])
 })
-$('.stop').on('click',function(){
+$('.stop').on('click', function() {
   owl.trigger('stop.owl.autoplay')
 })
